9506c273001bedf59f2d8f11c4478f806c4b7b08
Roosiel <rzagramonte@outlook.com>
2025-07-05 15:55:16 -0400

    Backend working: fixed Django env issues, installed psycopg2, cloudinary, and got server running

--- backend/book_backend/settings.py
+++ backend/book_backend/settings.py
@@ -11,15 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import environ
-
-env = environ.Env()
-environ.Env.read_env()  # loads .env file
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # loads .env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
